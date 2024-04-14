fac65a98d3600aba4445c37bab149f39633770e5
Mohamed Albasuony <97564022+mohamedalbasuony1@users.noreply.github.com>
2024-04-13 20:05:51 -0400

    Client side routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 import {
   createBrowserRouter,
   RouterProvider,
@@ -13,7 +14,16 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
-    
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,10 @@
+import { Outlet, Link } from "react-router-dom";
 export default function Root() {
     return (
       <>
         <div id="sidebar">
           <h1>React Router Contacts</h1>
+          
           <div>
             <form id="search-form" role="search">
               <input
@@ -29,15 +31,17 @@ export default function Root() {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+              <Link to={`contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+              <Link to={`contacts/2`}>Your Friend</Link>
               </li>
             </ul>
           </nav>
         </div>
-        <div id="detail"></div>
+        <div id="detail">
+        <Outlet />
+      </div>
       </>
     );
   }
